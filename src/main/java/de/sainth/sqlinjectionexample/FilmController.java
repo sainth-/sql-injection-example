4a7c043b8db45d799d23cc7b362a0aed3a417b36
Tobias Wink <sainth@sainth.de>
2019-05-29 09:26:14 +0200

    Add logger

--- src/main/java/de/sainth/sqlinjectionexample/FilmController.java
+++ src/main/java/de/sainth/sqlinjectionexample/FilmController.java
@@ -1,17 +1,19 @@
 package de.sainth.sqlinjectionexample;
 
-import java.sql.*;
-
-import javax.sql.DataSource;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.*;
+
 @RestController
 public class FilmController {
 
+  private static final Logger logger = LoggerFactory.getLogger(FilmController.class);
   private final DataSource dataSource;
 
   @Autowired
@@ -24,6 +26,7 @@ public class FilmController {
     int count = 0;
     try (Connection con = dataSource.getConnection(); Statement stmt = con.createStatement()) {
       String sql = "SELECT * FROM film WHERE title LIKE '%" + name + "%'";
+      logger.debug("getManuallyFilmCountByTitle : " + sql);
       ResultSet resultSet = stmt.executeQuery(sql);
       while (resultSet.next()) {
         count++;
@@ -37,6 +40,7 @@ public class FilmController {
     int count = 0;
     try (Connection con = dataSource.getConnection(); Statement stmt = con.createStatement()) {
       String sql = "SELECT COUNT(*) FROM film WHERE title LIKE '%" + name + "%'";
+      logger.debug("getFilmCountByTitle: " + sql);
       ResultSet resultSet = stmt.executeQuery(sql);
       if (resultSet.next()) {
         count = resultSet.getInt(1);
@@ -50,6 +54,7 @@ public class FilmController {
     int count = 0;
     try (Connection con = dataSource.getConnection(); PreparedStatement stmt = con.prepareStatement("SELECT COUNT(*) FROM film WHERE title LIKE ?")) {
       stmt.setString(1, "%" + name + "%");
+      logger.debug("getPreparedFilmCountByTitle: " + stmt);
       ResultSet resultSet = stmt.executeQuery();
       if (resultSet.next()) {
         count = resultSet.getInt(1);
